316cb03f2af1754c9065c415206a4d17ec013ffe
Luciano Jesus Rodriguez <luciano_rodriguez86@hotmail.com>
2024-12-30 09:47:26 -0300

    commit
    Proyecto realizado

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const toggleBtn = document.querySelector('.mobile-menu-icon');
-    const links = document.querySelector('.nav .links');
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.querySelector('.mobile-menu-icon');
+    const navLinks = document.querySelector('.nav .links');  // Cambiado a .links
 
-    // Manejador del evento click para mostrar/ocultar el menú
-    toggleBtn.addEventListener('click', function() {
-        links.classList.toggle('active');
+    menuToggle.addEventListener('click', function() {
+        navLinks.classList.toggle('active');
+    });
+
+    // Cerrar el menú al hacer clic en un enlace
+    const links = document.querySelectorAll('.nav .links li a');
+    links.forEach(function(link) {
+        link.addEventListener('click', function() {
+            navLinks.classList.remove('active');
+        });
     });
 });
+
